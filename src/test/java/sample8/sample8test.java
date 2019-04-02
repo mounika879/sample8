e4a59f124ac301efd83bfdcce0fb735c1034b319
mounika <kodatimounika@gmail.com>
2019-04-02 19:48:21 +0530

    this is sample8

---
+++ src/test/java/sample8/sample8test.java
@@ -0,0 +1,14 @@
+package sample8;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class sample8test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
